5762e7911ae5d52cc4b7fa30424df750af4e1e19
Mariano Joison <mariano.joison@spoton.com>
2023-01-20 08:05:40 -0500

    Agustin Joison - Basic Web UI Automation Test implementation

---
+++ Automation/WebUI/LoginTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace WebUI
+{
+    public class LoginTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void SeleniumFirstTest1()
+        {
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+            driver.FindElement(By.Id("username")).SendKeys("rahulshettyacademy");
+            driver.FindElement(By.Id("password")).SendKeys("learning");
+            driver.FindElement(By.Id("signInBtn")).Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.ClassName("navbar-brand")));
+
+            Assert.That(driver.FindElement(By.ClassName("navbar-brand")).Text, Is.EqualTo("ProtoCommerce"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+    }
+}
